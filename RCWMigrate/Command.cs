3aee23d11b0d66e8c1d5aecbfb65593ccbfd7311
Zhong Wu <wuzhong1980@gmail.com>
2024-04-02 16:18:12 -0400

    change the file name

---
+++ RCWMigrate/Command.cs
@@ -0,0 +1,143 @@
+﻿// (C) Copyright 2011 by Autodesk, Inc. 
+//
+// Permission to use, copy, modify, and distribute this software
+// in object code form for any purpose and without fee is hereby
+// granted, provided that the above copyright notice appears in
+// all copies and that both that copyright notice and the limited
+// warranty and restricted rights notice below appear in all
+// supporting documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK,
+// INC. DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL
+// BE UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is
+// subject to restrictions set forth in FAR 52.227-19 (Commercial
+// Computer Software - Restricted Rights) and DFAR 252.227-7013(c)
+// (1)(ii)(Rights in Technical Data and Computer Software), as
+// applicable.
+//
+
+using System;
+using System.IO;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.ApplicationServices;
+
+using DesignAutomationFramework;
+using Newtonsoft.Json;
+
+namespace ADNPlugin.Revit.RCWMigrator
+{
+    internal class RuntimeValue
+    {
+        // Change this to true when publishing to Revit IO cloud
+        public static bool RunOnCloud { get; } = true;
+    }
+
+
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class FileUpgradeApp : IExternalDBApplication
+    {
+        public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+        {
+            if (RuntimeValue.RunOnCloud)
+            {
+                DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+            }
+
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        public void HandleDesignAutomationReadyEvent( object sender, DesignAutomationReadyEventArgs e)
+        {
+            e.Succeeded = true;
+            UpgradeFile(e.DesignAutomationData);
+        }
+
+
+        protected void UpgradeFile( DesignAutomationData data )
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+
+            Application rvtApp = data.RevitApp;
+            if (rvtApp == null)
+                throw new InvalidDataException(nameof(rvtApp));
+
+            InputParams inputParams = InputParams.Parse("params.json");
+            if (inputParams == null)
+                throw new InvalidDataException("Cannot parse out input parameters correctly.");
+
+            Console.WriteLine("Got the input json file sucessfully.");
+
+            var cloudModelPath = ModelPathUtils.ConvertCloudGUIDsToCloudPath(ModelPathUtils.CloudRegionUS, inputParams.ProjectGuid, inputParams.ModelGuid);
+            //rvtApp.FailuresProcessing += OnFailuresProcessing;
+
+            Console.WriteLine("Revit starts openning Revit Cloud Model");
+            Document doc = rvtApp.OpenDocumentFile(cloudModelPath, new OpenOptions());
+            if (doc == null)
+                throw new InvalidOperationException("Could not open Revit Cloud Model.");
+
+            Console.WriteLine("Revit Cloud Model is opened");
+
+
+            doc.SaveAsCloudModel(inputParams.TargetAccountGuid, inputParams.TargetProjectGuid, inputParams.TargetFolderUrn, inputParams.TargetModelName);
+        }
+
+
+        public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
+        {
+
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        /// <summary>
+        /// InputParams is used to parse the input Json parameters
+        /// </summary>
+        internal class InputParams
+        {
+            public string Region { get; set; } = ModelPathUtils.CloudRegionUS;
+            [JsonProperty(PropertyName = "projectGuid", Required = Required.Default)]
+            public Guid ProjectGuid { get; set; }
+            [JsonProperty(PropertyName = "modelGuid", Required = Required.Default)]
+            public Guid ModelGuid { get; set; }
+
+            [JsonProperty(PropertyName = "TargetAccountGuid", Required = Required.Default)]
+            public Guid TargetAccountGuid { get; set; }
+
+            [JsonProperty(PropertyName = "TargetProjectGuid", Required = Required.Default)]
+            public Guid TargetProjectGuid { get; set; }
+
+            [JsonProperty(PropertyName = "TargetFolderUrn", Required = Required.Default)]
+            public string TargetFolderUrn { get; set; }
+
+
+            [JsonProperty(PropertyName = "TargetModelName", Required = Required.Default)]
+            public string TargetModelName { get; set; }
+
+
+            static public InputParams Parse(string jsonPath)
+            {
+                try
+                {
+                    if (!File.Exists(jsonPath))
+                        return new InputParams { Region = ModelPathUtils.CloudRegionUS, ProjectGuid = new Guid(""), ModelGuid = new Guid("") };
+
+                    string jsonContents = File.ReadAllText(jsonPath);
+                    return JsonConvert.DeserializeObject<InputParams>(jsonContents);
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine("Exception when parsing json file: " + ex);
+                    return null;
+                }
+            }
+        }
+
+    };
+
+}
--- RCWMigrate/Properties/AssemblyInfo.cs
+++ RCWMigrate/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FileUpgrader")]
+[assembly: AssemblyTitle("RCWMigrator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Autodesk, Inc.")]
-[assembly: AssemblyProduct("FileUpgrader")]
+[assembly: AssemblyProduct("RCWMigrator")]
 [assembly: AssemblyCopyright("Copyright © Autodesk, Inc. 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
